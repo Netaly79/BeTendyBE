8fc56790eb9954248fd8703e5dec5b748bb4f9a8
Karpenko, Nataliia (SHS TE DC SVK D&A IIT 1) <nataliia.karpenko@siemens-healthineers.com>
2025-10-18 18:13:37 +0200

    Add project files.

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeTendyBE.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet] public IActionResult Get() => Ok( new { status = "ok", time = DateTime.UtcNow });
+    }
+}
